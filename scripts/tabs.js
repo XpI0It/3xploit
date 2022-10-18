47c054b5506bcd778a3562b11abe59b6099917cc
batunpc <batuhnpc@gmail.com>
2022-10-18 02:41:52 -0400

    UI: enhancement main page

---
+++ scripts/tabs.js
@@ -0,0 +1,19 @@
+const tabs = document.querySelector(".wrapper");
+const tabButton = document.querySelectorAll(".tab-button");
+const contents = document.querySelectorAll(".content");
+
+tabs.onclick = (e) => {
+  const id = e.target.dataset.id;
+  if (id) {
+    tabButton.forEach((btn) => {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+
+    contents.forEach((content) => {
+      content.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+};
