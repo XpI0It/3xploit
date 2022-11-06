cc9825a624db6fd22173de79f547ed61eda09a03
vansh <vansh@mail.com>
2022-11-06 13:30:52 -0500

    dynamicLB bug fixed, username&Logout added

--- routes/main.js
+++ routes/main.js
@@ -3,13 +3,28 @@ const router = express.Router();
 
 //home
 router.get('/', (req, res) => {
-  res.render('home/home', {
-    layout: 'main',
-    title: 'Home',
-    // custom css file for this page
-    style: 'style.css',
-    script: 'scrolldown.js',
-  });
+	
+	if(req.session.user)
+	{
+		res.render('home/home', {
+			layout: 'main',
+			title: 'Home',
+			username: req.session.user.username,
+			// custom css file for this page
+			style: 'style.css',
+			script: 'scrolldown.js'
+		});
+	}
+	else
+	{
+		res.render('home/home', {
+			layout: 'main',
+			title: 'Home',
+			// custom css file for this page
+			style: 'style.css',
+			script: 'scrolldown.js'
+		});
+	}
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -4,143 +4,167 @@ const user = require('../schemas/UserSchema');
 
 // middleware function to check if user logged in or not
 function isAuthenticated(req, res, next) {
-  if (req.session.user) {
-    res.redirect('/game/3xploit-modules');
-  } else {
-    next();
+	if(req.session.user){
+	  res.redirect('/game/3xploit-modules')
+	}
+	else{
+	  next();
+	}
   }
-}
+
 
 // GET: Login route
 router.get('/login', isAuthenticated, (req, res) => {
-  res.render('client/login', {
-    title: 'Login',
-    style: 'auth.css',
-  });
+	res.render('client/login', {
+		title: 'Login',
+		style: 'auth.css'
+	});
+});
+
+
+router.get('/logout', (req,res) => {
+	req.session.destroy((data) => {
+		res.render('home/home', {
+			layout: 'main',
+			title: 'Home',
+			//username: req.session.user.username,
+			// custom css file for this page
+			style: 'style.css',
+			script: 'scrolldown.js'
+		});
+	});
 });
 
 // GET: Register route
 router.get('/register', isAuthenticated, (req, res) => {
-  res.render('client/register', {
-    title: 'Register',
-    style: 'auth.css',
-  });
+	res.render('client/register', {
+		title: 'Register',
+		style: 'auth.css'
+	});
 });
 
 // Post method for login form
 router.post('/login', async (req, res) => {
-  // Accessing data from body
-  var email = req.body.email;
-  var password = req.body.password;
+	// Accessing data from body
+	var email = req.body.email;
+	var password = req.body.password;
 
-  // console.log(req.sessionID)
+	// console.log(req.sessionID)
 
-  try {
-    user.findOne({ $and: [{ email: email }, { password: password }] }).then((user) => {
-      if (user != null) {
-        var username = user.username;
+	try {
+		user.findOne({ $and: [{ email: email }, { password: password }] }).then(
+			user => {
+				if (user != null) {
+					var username = user.username;
+					
+					// create a session for user
+					req.session.authenticated = true;
+					req.session.user = {
+						email, password, username
+					}
+					req.session.cookie.expires = false;
+					res.redirect('/game/3xploit-modules');
 
-        // create a session for user
-        req.session.authenticated = true;
-        req.session.user = {
-          email,
-          password,
-          username,
-        };
-        req.session.cookie.expires = false;
-        res.redirect('/game/3xploit-modules');
-      } else {
-        res.render('client/login', {
-          error: 'Invalid username or password',
-          title: 'Login',
-          style: 'auth.css',
-        });
-      }
-    });
-  } catch (e) {
-    console.log(e);
-  }
+				} else {
+					res.render('client/login', {
+						error: 'Invalid username or password',
+						title: 'Login',
+						style: 'auth.css'
+					});
+				}
+			}
+		);
+	} catch (e) {
+		console.log(e);
+	}
 });
 
-// make the post route better in efficiency
+// Post method for registration form
 router.post('/register', async (req, res) => {
-  let { email, username, password } = req.body;
-  if (checkNullOrEmpty(email, username, password)) {
-    res.render('client/register', {
-      error: 'Enter all the required fields below',
-      title: 'Register',
-      style: 'auth.css',
-    });
-  } else if (
-    !/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-      email
-    )
-  ) {
-    res.render('client/register', {
-      error: 'Entered email address is not valid',
-      title: 'Register',
-      style: 'auth.css',
-    });
-  } else if (await isUserExisting(email)) {
-    res.render('client/register', {
-      error: 'Email already exists !!',
-      title: 'Register',
-      style: 'auth.css',
-    });
-  } else if (await isUserNameTaken(username)) {
-    res.render('client/register', {
-      error: 'This username is already taken',
-      title: 'Register',
-      style: 'auth.css',
-    });
-  } else {
-    var newUser = new user({
-      email: email,
-      username: username,
-      password: password,
-    });
+	var email = req.body.email;
+	var password = req.body.password;
+	var username = req.body.username;
 
-    try {
-      await newUser.save();
-      console.log('User registered successfully');
+	if (checkNullOrEmpty(email, username, password)) {
+		res.render('client/register', {
+			error: 'Enter all the required fields below',
+			title: 'Register',
+			style: 'auth.css'
+		});
+	} else if (
+		/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+			email
+		) == false
+	) {
+		res.render('client/register', {
+			error: 'Entered email address is not valid',
+			title: 'Register',
+			style: 'auth.css'
+		});
+	} else if (await isUserExisting(email)) {
+		res.render('client/register', {
+			error: 'Email already exists !!',
+			title: 'Register',
 
-      req.session.authenticated = true;
-      req.session.user = {
-        email,
-        password,
-        username,
-      };
-      req.session.cookie.expires = false;
-      res.redirect('/game/3xploit-modules');
-    } catch (e) {
-      console.log(e);
-    }
-  }
+			style: 'auth.css'
+		});
+	} else if (await isUserNameTaken(username)) {
+		res.render('client/register', {
+			error: 'This username is already taken',
+			title: 'Register',
+			style: 'auth.css'
+		});
+	} else {
+		var newUser = new user({
+			email: email,
+			username: username,
+			password: password
+		});
+
+		try {
+			await newUser.save().then(mssg => {
+				console.log('User registered successfully');
+
+				// creates a session for the new user
+				req.session.authenticated = true;
+					req.session.user = {
+						email, password, username
+					}
+					req.session.cookie.expires = false;
+
+				res.redirect('/game/3xploit-modules');
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	}
 });
 
-const checkNullOrEmpty = (mail, username, password) => {
-  var check1 = mail === '' || mail === undefined;
-  var check2 = username === '' || username === undefined;
-  var check3 = password === '' || password === undefined;
+// checks if form data is valid or not
+function checkNullOrEmpty(mail, username, password) {
+	// check if data is null or empty
+	var check1 = mail === '' || mail === undefined;
+	var check2 = username === '' || username === undefined;
+	var check3 = password === '' || password === undefined;
 
-  if (check1 || check2 || check3) return true;
-  else return false;
-};
+	if (check1 || check2 || check3) return true;
+	else return false;
+}
 
 // checks if email in database already exists or not
 async function isUserExisting(email) {
-  return user.findOne({ email: email }).then((score) => {
-    if (score != null) return true;
-    else return false;
-  });
+	return user.findOne({ email: email }).then(score => {
+		if (score != null) return true;
+		else return false;
+	});
 }
 
 // checks if username is already taken by someone or not
 async function isUserNameTaken(username) {
-  return user.findOne({ username: username }).then((data) => {
-    if (data != null) return true;
-    else return false;
-  });
+	return user.findOne({ username: username }).then(data => {
+		if (data != null) return true;
+		else return false;
+	});
 }
 
 module.exports = router;
